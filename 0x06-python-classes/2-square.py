c365cf7a6f8e5dc366934308719559eb3a0ce053
Dominic Msukwa <domimsukwa.dm@gmail.com>
2024-01-05 01:39:54 +0000

    revise solution 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 
+"""Creates square based on 1-square.py"""
+
+
 class Square:
+    """Square class definition"""
     def __init__(self, size=0):
+        """constructor with value checking"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer.")
         elif size < 0:
