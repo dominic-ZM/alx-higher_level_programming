8641c90b89283eb6a39a5926c86e36415cff0d2e
Dominic Msukwa <domimsukwa.dm@gmail.com>
2024-01-05 02:37:55 +0000

    revise pycodestyle error in 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -31,7 +31,7 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
+
     def is_valid_tuple(self, value):
         """a function to check if a value is a valid tuple"""
         if not isinstance(value, tuple):
